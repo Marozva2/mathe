bfac1d5c3dcc3243fa7cf8a9a33da8fb85b2ef25
Frankline Isaboke <isabokefrankline@gmail.com>
2024-03-17 15:29:08 +0300

    routed components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,23 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LandingPage from "./components/LandingPage";
+import Dashboard from "./components/Dashboard";
+import LogIn from "./components/Login";
+import Register from "./components/Register";
+import Logout from "./components/Logout";
 
 function App() {
-  return(
-  <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/login" element={<LogIn />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/logout" element={<Logout />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
