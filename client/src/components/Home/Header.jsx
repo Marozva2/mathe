d7b1b8941772abb70bea3a2d7eca32bf9e264f4c
Marozva2 <isabokefrankline@gmail.com>
2024-07-21 18:09:22 +0300

    Re-modified the landing page to feature the new design

--- client/src/components/Home/Header.jsx
+++ client/src/components/Home/Header.jsx
@@ -1,21 +1,21 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
+
+const linkClasses = "text-[#6b4146] no-underline text-2xl font-extrabold";
 
 function Header() {
   return (
-    <header className="bg-blue-500 text-white">
-      <nav className="container mx-auto p-4 flex justify-between items-center">
-        <div className="text-xl font-bold">
-          <Link to="/">LOGO</Link>
+    <div className="w-full bg-[#56dffe] py-4 px-10 flex items-center justify-between">
+      <div className="text-xl font-bold text-black">LOGO</div>
+      <div className="flex-grow flex justify-center">
+        <div className="space-x-8 mt-12">
+          <Link to="/" className={linkClasses}>Home</Link>
+          <Link to="/services" className={linkClasses}>Services</Link>
+          <Link to="/about-us" className={linkClasses}>About Us</Link>
+          <Link to="/contact-us" className={linkClasses}>Contact Us</Link>
         </div>
-        <ul className="flex space-x-4">
-          <li><Link to="/" className="hover:underline">Home</Link></li>
-          <li><Link to="/services" className="hover:underline">Services</Link></li>
-          <li><Link to="/about-us" className="hover:underline">About Us</Link></li>
-          <li><Link to="/register" className="hover:underline">Register/Login</Link></li>
-        </ul>
-      </nav>
-    </header>
+      </div>
+    </div>
   );
 }
 
--- client/src/components/Home/LandingPage.jsx
+++ client/src/components/Home/LandingPage.jsx
@@ -1,23 +1,13 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
-import NavBar from "./NavBar";
-import Footer from "./Footer";
+import Header from "./Header";
 import Home from "./Home";
-import About from "./About";
-import Services from "../Services/Services";
-import Testimonials from "./Testimonials";
 
 function LandingPage() {
   return (
     <>
-      <NavBar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-      </Routes>
-      <Services />
-      <Testimonials />
-      <Footer />
+     <Header />
+     <Home/>
     </>
   );
 }
