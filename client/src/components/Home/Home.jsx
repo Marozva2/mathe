9f2e2b7808ee6d9f046eedf7b18a116bc3a59bad
Marozva2 <isabokefrankline@gmail.com>
2024-07-22 14:24:30 +0300

    redirection to the register page

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,9 +1,16 @@
-import React from 'react';
-import Header from './Header';
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import Header from "./Header";
 
 function Home() {
+  const navigate = useNavigate();
+
+  const handleButtonClick = () => {
+    navigate("/register");
+  };
+
   return (
-    <div className='bg-[#56dffe] min-h-screen flex flex-col items-center'>
+    <div className="bg-[#56dffe] min-h-screen flex flex-col items-center">
       <Header />
       <div className="text-white text-3xl italic m-20 text-center">
         Welcome to <span className="font-bold">Mathe</span>
@@ -13,7 +20,10 @@ function Home() {
         door-to-door <br />
         laundry service!
       </div>
-      <button className="bg-[#6b4146] text-white text-xl px-6 py-3 rounded-lg">
+      <button
+        onClick={handleButtonClick}
+        className="bg-[#6b4146] text-white text-xl px-6 py-3 rounded-lg"
+      >
         Let's Get Started
       </button>
     </div>
