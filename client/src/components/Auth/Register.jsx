606b2dd83949ef8d4f84a3065acd383d97efa0f5
Marozva2 <isabokefrankline@gmail.com>
2024-07-23 09:46:02 +0300

    Remodified Register page's UI to match the design

--- client/src/components/Auth/Register.jsx
+++ client/src/components/Auth/Register.jsx
@@ -1,162 +1,148 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import Header from "../Home/Header";
+import image from "../../assets/images/image.png";
 
 function Register() {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
   const [email, setEmail] = useState("");
-  const [confirmPassword, setConfirmPassword] = useState("");
+  const [name, setName] = useState("");
+  const [contact, setContact] = useState("");
+  const [password, setPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
   const [error, setError] = useState(null);
-  const [formValid, setFormValid] = useState(false);
   const navigate = useNavigate();
 
-  useEffect(() => {
-    checkFormValidity();
-  }, [username, password, email, confirmPassword]);
-
-  const handleSignUp = async (event) => {
+  const handleSubmit = async (event) => {
     event.preventDefault();
-
-    if (!formValid) {
-      setError("Please fill in all required fields");
-      return;
-    }
-
-    if (password !== confirmPassword) {
-      setError("Passwords do not match");
-      return;
-    }
-
     try {
+      const formData = { email, password };
+
       const response = await fetch("http://127.0.0.1:5000/register", {
         method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          username,
-          email,
-          password,
-          confirmPassword,
-        }),
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(formData),
       });
+
       const data = await response.json();
+      console.log(data);
+
       if (response.ok) {
-        setUsername("");
-        setEmail("");
-        setPassword("");
-        setConfirmPassword("");
-        setError(null);
-        navigate("/login");
+        if (data.access_token) {
+          localStorage.setItem("accessToken", data.access_token);
+          localStorage.setItem("userId", data.user_id);
+          setError(null);
+          navigate("/dashboard"); // Redirect to dashboard on successful login
+          return; // Exit the function after redirection
+        } else {
+          setError("Invalid email or password.");
+        }
       } else {
-        setError(data.detail);
+        setError("An error occurred. Please try again later.");
       }
     } catch (err) {
-      console.error(err);
-    }
-  };
-
-  const checkFormValidity = () => {
-    if (
-      username.trim() !== "" &&
-      email.trim() !== "" &&
-      password.trim() !== "" &&
-      confirmPassword.trim() !== ""
-    ) {
-      setFormValid(true);
-    } else {
-      setFormValid(false);
-    }
-  };
-
-  const handleInputChange = (e) => {
-    const { name, value } = e.target;
-    switch (name) {
-      case "username":
-        setUsername(value);
-        break;
-      case "email":
-        setEmail(value);
-        break;
-      case "password":
-        setPassword(value);
-        break;
-      case "confirmPassword":
-        setConfirmPassword(value);
-        break;
-      default:
-        break;
+      console.error("Login error:", err);
+      setError("An error occurred. Please try again later.");
     }
   };
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-      <form
-        onSubmit={handleSignUp}
-        className="w-full max-w-md mt-16 bg-white rounded-lg shadow-md overflow-hidden"
-      >
-        <div className="px-6 py-4">
-          <h2 className="text-4xl font-bold text-center text-gray-700 mb-8">
-            Sign Up
-          </h2>
-          <input
-            type="email"
-            placeholder="Email"
-            name="email"
-            value={email}
-            onChange={handleInputChange}
-            required
-            className="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
-          />
-          <input
-            type="text"
-            placeholder="Username"
-            name="username"
-            value={username}
-            onChange={handleInputChange}
-            required
-            className="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
-          />
-          <input
-            type="password"
-            placeholder="Password"
-            name="password"
-            value={password}
-            onChange={handleInputChange}
-            required
-            className="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
-          />
-          <input
-            type="password"
-            placeholder="Confirm Password"
-            name="confirmPassword"
-            value={confirmPassword}
-            onChange={handleInputChange}
-            required
-            className="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
-          />
+    <>
+      <div className="h-screen bg-[#55d9ff]">
+        <Header />
+        <div className="flex">
+          <div className="w-1/2 flex justify-center p-5">
+            <form onSubmit={handleSubmit} className="w-full max-w-md">
+
+              <h1 className="text-[#6c3838] text-center font-bold pb-4">
+                Create your account <br /> today to get a <br /> Perfect Wash
+              </h1>
+
+              <input
+                id="name"
+                name="name"
+                type="text"
+                placeholder="Name:"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                required
+                className="w-full p-2 mb-4 rounded bg-[#6c3838]"
+                autoComplete="name"
+              />
+
+              <input
+                id="contact"
+                name="contact"
+                type="number"
+                placeholder="Contact:"
+                value={contact}
+                onChange={(e) => setContact(e.target.value)}
+                required
+                className="w-full p-2 mb-4 rounded bg-[#6c3838]"
+                autoComplete="contact"
+              />
+
+              <input
+                id="email"
+                name="email"
+                type="email"
+                placeholder="Email:"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                required
+                className="w-full p-2 mb-4 bg-[#6c3838] rounded"
+                autoComplete="email"
+              />
+
+              <input
+                id="password"
+                name="password"
+                type={showPassword ? "text" : "password"}
+                placeholder="Password:"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+                className="w-full p-2 mb-4 bg-[#6c3838] rounded"
+                autoComplete="current-password"
+              />
+
+              {/* <button
+                type="button"
+                onClick={() => setShowPassword(!showPassword)}
+                className="w-full p-2 mb-4 bg-[#6c3838] text-white rounded hover:bg-blue-600"
+              >
+                {showPassword ? "Hide" : "Show"} Password
+              </button> */}
+
+              <div className="font-bold">
+                <input
+                  type="checkbox"
+                  id="checkbox"
+                  name="checkbox"
+                  required
+                  className="mr-2"
+                />
+                I have read the terms and conditions
+              </div>
+
+              <div className="w-40 flex items-center justify-center p-2 ml-16 mt-12 bg-[#6c3838] text-white rounded hover:bg-blue-600 font-bold">
+                <button type="submit">Create Account</button>
+              </div>
+              {error && (
+                <p className="text-red-500 text-center mt-4">{error}</p>
+              )}
+            </form>
+          </div>
+          <div className="w-1/2 flex justify-center">
+            <img
+              src={image}
+              alt="Illustration"
+              className="object-cover"
+              style={{ width: 619, height: 670 }}
+            />
+          </div>
         </div>
-        <div className="px-6 py-4 bg-gray-50">
-          <button
-            type="submit"
-            className="w-full py-2 px-4 text-center bg-blue-500 hover:bg-blue-700 text-white font-bold rounded"
-          >
-            Sign Up
-          </button>
-          <p className="text-center mt-4">
-            Already have an account?{" "}
-            <a
-              href="/login"
-              className="text-blue-500 hover:text-blue-700 underline"
-            >
-              Sign in
-            </a>{" "}
-            here!
-          </p>
-        </div>
-      </form>
-      {error && <p className="text-red-500">{error}</p>}
-    </div>
+      </div>
+    </>
   );
 }
 
