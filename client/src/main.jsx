4dd012464df37c829e0d9a65dfe546dfa08ee071
Marozva2 <isabokefrankline@gmail.com>
2024-08-08 15:39:35 +0300

    provided authorization for both the admin and user

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,10 +4,13 @@ import App from "./App.jsx";
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
-import 'remixicon/fonts/remixicon.css';
+import "remixicon/fonts/remixicon.css";
+import { AuthProvider } from "./components/Auth/AuthContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <AuthProvider>
+      <App />
+    </AuthProvider>
   </React.StrictMode>
 );
